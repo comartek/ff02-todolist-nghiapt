14d59c6ff836a6660edb45dc536f5a51133a31fd
nakde <111269414+nakde@users.noreply.github.com>
2022-08-22 09:03:45 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,90 @@
-import React, { useState } from "react";
 import "./App.css";
-
+import { useEffect, useRef, useState } from "react";
+import Table from "./components/Table/Table";
+import { getLocalStored, setLocalStored } from "./components/localStored";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  
+  // lấy user từ localstored sau nó chọc vào BIẾN email trong đó và lấy chính BIẾN đó làm key để thêm các jobs vào localstored
+  const user = getLocalStored("user");
+  const key = user?.email;
+  ///////////////
+  const [job, setJob] = useState({ content: "", dateline: "" });
+  const [jobs, setJobs] = useState(getLocalStored(key) || []);
+  const ref = useRef(null);
+  const ref2 = useRef(null);
+  const navigate = useNavigate();
+  /// change job in useState
+  const onSubmit = (e) => {
+    setJob({ ...job, [e.target.name]: e.target.value });
+  };
+  ///////////
+  ////// push job to useState jobs
+  const handleJobs = () => {
+    if (job.content === "") {
+      return;
+    }
+    if (job.dateline === "") {
+      return;
+    }
+    let arr = jobs;
+    setJobs(arr.concat(job));
+    setJob({ content: "", dateline: "" });
+    ref.current.value = "";
+    ref2.current.value = "";
+  };
+  //////////
+  // set jobs in local stored
+  useEffect(() => {
+    setLocalStored(key, jobs);
+    ref.current.focus();
+  }, [jobs]);
+  /////////////
+  // delete job in useSate jobs
+  const handleDelete = (index) => {
+    let deleteJobs = jobs;
+    deleteJobs.splice(index, 1);
+    setJobs(deleteJobs.concat());
+  };
+  ////////
+  // logic logout and check
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+  /////////////
   return (
-    <div className="app">
-      <h1>app</h1>
+    <div className="App">
+      <h1>TODOLISH for {user ? user?.email : null} </h1>
+
+      <button style={{ marginBottom: 30 }} onClick={handleLogout}>
+        logout
+      </button>
+      <br />
+      <input
+        name="content"
+        type="text"
+        placeholder="thêm công việc..."
+        onChange={onSubmit}
+        ref={ref}
+      />
+      <input
+        name="dateline"
+        style={{ marginLeft: 20 }}
+        type="date"
+        onChange={onSubmit}
+        ref={ref2}
+      />
+      <button style={{ marginLeft: 20 }} onClick={handleJobs}>
+        ADD
+      </button>
+      <Table jobs={jobs} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,131 @@
-import React from 'react'
-import styled from 'styled-components'
-import Input from './Input'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { users } from "../../config/users";
+import { setLocalStored, getLocalStored } from "../localStored";
 
 export default function Login() {
+  // set formik
+  let navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Please enter email"),
+      password: Yup.string().required("Please enter password"),
+    }),
+    ////
+    //set user login to localstored
+    onSubmit: (values) => {
+      const user = users.filter((props) => props.email === values.email);
+
+      if (user.length === 1) {
+        if (user[0].password === values.password) {
+          navigate("app");
+          alert("login success");
+          setLocalStored("user", user[0]);
+        } else {
+          alert("password is not correct, please type again");
+        }
+      } else {
+        alert("email is not correct, please type again");
+      }
+    },
+  });
+  ///////
+  // logic chuyen trang
+  const user = getLocalStored("user");
+  useEffect(() => {
+    if (user) {
+      navigate("/app");
+    }
+  }, []);
+  //////////
   return (
-    <Container>
+    <Contaner>
       <Wrapper>
-        <Input/>
+        <h1>SIGN IN</h1>
+        <form className="form" onSubmit={formik.handleSubmit}>
+          <StyledInput
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            placeholder="Enter Your Email"
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <span className="error">{formik.errors.email}</span>
+          ) : null}
+          <StyledInput
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            placeholder="Enter Your Password"
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <span className="error">{formik.errors.password}</span>
+          ) : null}
+
+          <button type="submit">Login</button>
+        </form>
       </Wrapper>
-    </Container>
-  )
+    </Contaner>
+  );
 }
 
-const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-`
+const Contaner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: lightcyan;
+  height: 100vh;
+`;
 const Wrapper = styled.div`
-    backdrop-filter: blur(35px);
-    background-color: rgba(255, 255, 255, 0.8);
-    height: 270px;
-    width: 300px;
-    border-radius: 20px;
-`
+  backdrop-filter: blur(35px);
+  background-color: rgba(255, 255, 255, 0.8);
+  height: 270px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 20px;
+  h1 {
+    margin-top: 1rem;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  button {
+    width: 80%;
+    max-width: 350px;
+    min-width: 250px;
+    height: 35px;
+    margin: 1rem 0;
+    border-radius: 8px;
+    background-color: #dedede;
+    color: #fff;
+    font-weight: 600;
+    cursor: pointer;
+  }
+`;
+const StyledInput = styled.input`
+  height: 25px;
+  max-width: 400px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 0 30px;
+  margin: 8px;
+`;
